1060c7245b3ac5e4e7b9d09baacbf223ba9df8e7
kocfurkan <koc_furkan@outlook.com>
2023-11-21 00:24:59 +0300

    added react app and some practice.

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
@@ -25,10 +27,17 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:5173");
+});
+
 app.UseAuthorization();
 
 app.MapControllers();
 
+
+//Another way to do this ??
 var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetService<StoreContext>();
 var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
