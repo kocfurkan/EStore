041110bf7cc330712582e08f1704aefc023358e3
kocfurkan <koc_furkan@outlook.com>
2024-01-30 23:53:31 +0300

    appsettings removed for security docker file configured new build added

--- client/src/features/catalog/ProductList.tsx
+++ client/src/features/catalog/ProductList.tsx
@@ -1,19 +1,26 @@
 ﻿import { Grid } from "@mui/material";
+import { Product } from "../../app/models/product";
 import ProductCard from "./ProductCard";
 import { useAppSelector } from "../../app/store/configureStore";
-import { Props } from "./ProducList";
+import ProductSkeleton from "./ProductSkeleton";
 
+interface Props {
+  products: Product[];
+}
 
 export default function ProductList({ products }: Props) {
-    const { productsLoaded } = useAppSelector(state => state.catalog);
-    return (
-
-        <Grid container spacing={4}>{products.map((product) => (
-            <Grid item xs={4} key={product.id}>
-                {!productsLoaded ? (<ProductSekeleton></ProductSekeleton>) : }
-                <ProductCard product={product}></ProductCard>
-            </Grid>
-        ))}
+  const { productsLoaded } = useAppSelector((state) => state.catalog);
+  return (
+    <Grid container spacing={4}>
+      {products.map((product) => (
+        <Grid item xs={4} key={product.id}>
+          {!productsLoaded ? (
+            <ProductSkeleton></ProductSkeleton>
+          ) : (
+            <ProductCard product={product}></ProductCard>
+          )}
         </Grid>
-    );
+      ))}
+    </Grid>
+  );
 }
